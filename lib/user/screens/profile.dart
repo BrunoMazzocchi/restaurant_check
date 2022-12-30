f0bb113454990d3808b52c958caed139db0d2225
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-12-29 18:47:18 -0600

    Profile

--- lib/user/screens/profile.dart
+++ lib/user/screens/profile.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_check/user/screens/settings.dart';
 import 'package:restaurant_check/user/screens/sign_in_view.dart';
+import 'package:shimmer/shimmer.dart';
+
 import '../../cart/widgets/profile_orders.dart';
 import '../domain/bloc/user_bloc.dart';
 import '../domain/models/user_model.dart';
@@ -14,10 +16,8 @@ class Profile extends StatefulWidget {
 }
 
 class _Profile extends State<Profile> {
-
-  late Future<User?> userData = Provider.of<UserBloc>(context, listen: false)
-      .fetchUser();
-
+  late Future<User?> userData =
+      Provider.of<UserBloc>(context, listen: false).fetchUser();
 
   @override
   Widget build(BuildContext context) {
@@ -25,119 +25,231 @@ class _Profile extends State<Profile> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
+    Widget shimmerUser() {
+      return Shimmer(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.grey[300]!,
+              Colors.grey[100]!,
+              Colors.grey[300]!,
+            ],
+          ),
+          child: Column(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: width,
+                    height: height * 0.3,
+                    decoration: const BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Settings(),
+                                ),
+                              );
+                            },
+                            icon: const Icon(
+                              Icons.more_horiz_outlined,
+                              size: 30,
+                            ),
+                            color: Colors.white,
+                          ),
+                          Container(
+                            width: 125,
+                            height: 125,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.circular(100),
+
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              userBloc.logOutUser();
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                      const SignInView()));
+
+                              SnackBar snackBar = const SnackBar(
+                                content: Text('Logged out'),
+                                duration: Duration(seconds: 2),
+                              );
+
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            },
+                            icon: const Icon(
+                              Icons.logout,
+                              size: 30,
+                            ),
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      const Text(
+                        'Loading',
+                        style:  TextStyle(
+                          fontFamily: "SF Pro",
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text(
+                        'Loading',
+                        style:  TextStyle(
+                            fontFamily: "SF Pro",
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const ProfileOrders(),
+            ],
+          )
+      );
+    }
+
     return FutureBuilder<User?>(
         future: userData,
         builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-            case ConnectionState.active:
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-              break;
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        width: width,
-                        height: height * 0.3,
-                        decoration: const BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10),
-                          ),
+          if (snapshot.hasData) {
+            Column(
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: width,
+                      height: height * 0.3,
+                      decoration: const BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
                         ),
                       ),
-                      Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              IconButton(
-                                onPressed: () {
-                                  Navigator.push(
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const Settings(),
+                                  ),
+                                );
+                              },
+                              icon: const Icon(
+                                Icons.more_horiz_outlined,
+                                size: 30,
+                              ),
+                              color: Colors.white,
+                            ),
+                            Container(
+                              width: 125,
+                              height: 125,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(100),
+                                image: DecorationImage(
+                                  image:
+                                      NetworkImage('${snapshot.data?.image}'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                userBloc.logOutUser();
+                                Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                      builder: (context) => const Settings(),
-                                    ),
-                                  );
-                                },
-                                icon: const Icon(
-                                  Icons.more_horiz_outlined,
-                                  size: 30,
-                                ),
-                                color: Colors.white,
-                              ),
-                              Container(
-                                width: 125,
-                                height: 125,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  border: Border.all(
-                                    color: Colors.white,
-                                    width: 2,
-                                  ),
-                                  borderRadius: BorderRadius.circular(100),
-                                  image: DecorationImage(
-                                    image:
-                                        NetworkImage('${snapshot.data?.image}'),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              IconButton(
-                                onPressed: () {
-                                  userBloc.logOutUser();
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => const SignInView()));
+                                        builder: (context) =>
+                                            const SignInView()));
 
-                                  SnackBar snackBar = const SnackBar(
-                                    content: Text('Logged out'),
-                                    duration: Duration(seconds: 2),
-                                  );
+                                SnackBar snackBar = const SnackBar(
+                                  content: Text('Logged out'),
+                                  duration: Duration(seconds: 2),
+                                );
 
-                                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                                },
-                                icon: const Icon(
-                                  Icons.logout,
-                                  size: 30,
-                                ),
-                                color: Colors.white,
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              },
+                              icon: const Icon(
+                                Icons.logout,
+                                size: 30,
                               ),
-                            ],
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          '${snapshot.data?.name}',
+                          style: const TextStyle(
+                            fontFamily: "SF Pro",
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            '${snapshot.data?.name}',
-                            style: const TextStyle(
+                        ),
+                        Text(
+                          '${snapshot.data?.nickname}',
+                          style: const TextStyle(
                               fontFamily: "SF Pro",
                               color: Colors.white,
                               fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '${snapshot.data?.nickname}',
-                            style: const TextStyle(
-                                fontFamily: "SF Pro",
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.normal),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const ProfileOrders(),
-                ],
-              );
-              break;
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                const ProfileOrders(),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return  shimmerUser();
+
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
           return const Center(
             child: CircularProgressIndicator(),
